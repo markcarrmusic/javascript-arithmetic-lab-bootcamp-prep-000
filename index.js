a8e27e2a03534758e801aa73fce5965b8b08d400
markcarrmusic@gmail.com <markcarrmusic@gmail.com>
2018-04-13 02:38:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Basic math functions
 function add(a, b) {
-  a + b
+ return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide(a ,b) {
+  return a / b
 }
